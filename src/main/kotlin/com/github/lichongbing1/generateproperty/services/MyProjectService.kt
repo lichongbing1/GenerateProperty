d9bc7347bf8045d125f12ba2cffa87567568e7b3
GitHub Action <action@github.com>
2024-06-22 01:35:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/lichongbing1/generateproperty/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lichongbing1/generateproperty/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lichongbing1.generateproperty.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lichongbing1.generateproperty.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
